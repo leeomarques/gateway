ca900ae862fd2cac5375852da62a36781abfbee7
leeomarques <l30n4rd0@301090>
2023-12-27 21:28:03 -0300

    asdas

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -9,13 +9,22 @@ import TranslateDirective from './language/translate.directive';
 import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
 
-import { ButtonModule } from 'primeng/button';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 /**
  * Application wide Module
  */
 @NgModule({
-  imports: [AlertComponent, AlertErrorComponent, FindLanguageFromKeyPipe, TranslateDirective],
+  imports: [
+    AlertComponent,
+    AlertErrorComponent,
+    FindLanguageFromKeyPipe,
+    TranslateDirective,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   exports: [
     CommonModule,
     NgbModule,
